11b3e5c8cbcb86045b4f0cd6ce2f81a883c03289
vehre <vehre@gmx.de>
2014-09-19 15:12:22 +0200

    Removed sgminer.

--- sgminer-master/bin/SGMiner_FTC_1.bat
+++
@@ -1,5 +0,0 @@
-setx DISPLAY 0
-setx GPU_MAX_ALLOC_PERCENT 100
-setx GPU_USE_SYNC_OBJECTS 1
-
-sgminer --algorithm neoscrypt --worksize 128 --thread-concurrency 200 -I 8 -o stratum+tcp://coinotron.com:3337 -u cqtenq.1 -p 110
